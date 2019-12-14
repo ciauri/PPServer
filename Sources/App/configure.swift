e77aeba7d62cb3f03e9c2e59910a5d57137c75cb
Stephen Ciauri <stephen.ciauri@gmail.com>
2019-12-14 14:05:51 -0800

    Use a persistent db and remove template nonsense

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,7 +18,12 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    let sqlite: SQLiteDatabase
+    if let sqlitePath = Environment.get("PPS_SQLITE_DB") {
+        sqlite = try SQLiteDatabase(storage: .file(path: sqlitePath))
+    } else {
+        sqlite = try SQLiteDatabase(storage: .memory)
+    }
 
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
